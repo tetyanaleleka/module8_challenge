e7e53effecd926cf62030b8a534edf47e0c2842c
tetyanaleleka <tana.leleka@gmail.com>
2023-09-18 20:10:50 +0100

    The API was added successfully

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 
+const weatherAPIURL = "https://api.openweathermap.org"
+const weatherAPIKey = "e4e71f36d86d2752757d45a1af59f34a"
+
 
 let searchInput = $("#search-input")
-let searchInput = $("#search-form");
+let searchForm = $("#search-form");
 
 
-function submitSearchForm()
+function fetchCoord(search){
+    let queryURL = `${weatherAPIURL}/geo/1.0/direct?q=${search}&limit=5&appid=${weatherAPIKey}`
+    console.log(queryURL);
+    fetch(queryURL).then(function(data){
+        return data.json()
+    }).then(function(response){
+        console.log(response);
+    })
 
+
+}
+
+function submitSearchForm(event){
+
+
+
+    event.preventDefault();
+    let search = searchInput.val().trim()
+
+    fetchCoord(search);
+
+}
+
+
+searchForm.on("submit", submitSearchForm);
 
 
 
